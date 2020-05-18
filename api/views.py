cea2b9db9020fc7d4b32f855627964a21d889fc2
RajapandiR <rajapandibsc12@gmail.com>
2020-05-18 14:47:22 +0530

    Added post, put, patch and delete

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from api import serializers
+from rest_framework import status
 # Create your views here.
 
 class HelloApiView(APIView):
-
+	serializer_class = serializers.HelloSerializers
 	def get(self, request, format = None):
 
 		views = [
@@ -13,3 +15,24 @@ class HelloApiView(APIView):
 		]
 
 		return Response({'message': 'Hello', 'views':views})
+
+	def post(self, request):
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello {name}'
+			return Response({'message':message})
+		else:
+			return Response(
+				serializer.errors,
+				status = status.HTTP_400_BAD_REQUEST
+				)
+
+	def put(self, request):
+		return Response({'message': 'PUT'})
+
+	def patch(self, request):
+		return Response({'message': 'PATCH'})
+
+	def delete(self, request):
+		return Response({'message': 'DELETE'})
