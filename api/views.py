c600411cd84343334b30533cadc827a9d6e0e2de
RajapandiR <rajapandibsc12@gmail.com>
2020-05-18 13:00:27 +0530

    Added Hello Api view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+
+	def get(self, request, format = None):
+
+		views = [
+			'User HTTP Methods as (get, post, put, delete)',
+
+		]
+
+		return Response({'message': 'Hello', 'views':views})
