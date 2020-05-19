120f61a0d1f2c28155daf008b6324508c5fb19e5
RajapandiR <rajapandibsc12@gmail.com>
2020-05-19 11:20:50 +0530

    Added list, create, update viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
 from api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('Hello-viewset', views.HelloViewSet, basename='Hello-viewset')
+
 urlpatterns = [
-	path('Hello-view/', views.HelloApiView.as_view())
+	path('Hello-view/', views.HelloApiView.as_view()),
+	path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
-from api import serializers
 from rest_framework import status
+from api import serializers
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -36,3 +38,31 @@ class HelloApiView(APIView):
 
 	def delete(self, request):
 		return Response({'message': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+	serializer_class = serializers.HelloSerializers
+
+	def list(self, request):
+		viewset = [
+			'User actions create, read, update, destroy, list'
+		]
+		return Response({'message': 'Hello', 'viewset': viewset})
+
+	def create(self, request):
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello {name}'
+			return Response({'message':message})
+		else:
+			return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+	def retrieve(self, request, pk=None):
+		return Response({'HTTP-Method': 'GET'})
+		
+	def update(self, request, pk=None):
+		return Response({'HTTP-Method': 'PUT'})
+
+	def destroy(self, request, pk=None):
+		return Response({'HTTP-Method': 'DELETE'})
